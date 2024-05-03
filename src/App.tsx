671a5f54e9dad128c426faf1c51222ceec93e4ef
denizafontineles <denizafontineles@outlook.com>
2024-05-03 14:11:09 -0300

    cria toda estrutura de temas e reconfigura a lógica dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,34 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Cadastro from "./pages/cadastro/Cadastro";
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/temas/listaTemas/ListaTemas";
+import FormularioTema from "./components/temas/formularioTema/FormularioTema";
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-  </>
-  )
+        </AuthProvider>
+    </>
+  );
 }
-
 export default App;
